d5dfef96fe9343715243d13c002ff78d81148a8b
kartik pansuriya <114102632+KARTIKPANSURIYA@users.noreply.github.com>
2024-12-13 23:25:27 -0500

    Update ui_helpers.py

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -27,4 +27,10 @@ def fade_out(widget, steps=10, interval=50):
     Args:
         widget (tk.Widget): The tkinter widget to animate.
         steps (int): The number of steps for the fade-out effect.
-        interval (int): The time interval (ms) between
+        interval (int): The time interval (ms) between each step.
+    """
+    for i in range(steps + 1):
+        alpha = 1 - (i / steps)
+        widget.master.attributes("-alpha", alpha)
+        widget.master.update_idletasks()
+        widget.after(interval)
